35a7a1c917cf6f2ce24c289aa86bbf82e52eaf96
Arold Ngouani Yapteu <aroldngouani@MacBook-Air-de-Arold.local>
2023-12-20 10:33:18 +0100

    Gestion des donnees avec un fichier go et gestion des erreur

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,34 +1,78 @@
 package dictionary
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 )
 
-type Dictionary map[string]string
-
-func New() Dictionary {
-	return Dictionary{}
+type Word struct {
+	Mot        string `json:"mot"`
+	Definition string `json:"definition"`
 }
 
-func (d Dictionary) Add(mot, definition string) {
-	d[mot] = definition
+type Dictionary struct {
+	Entries []Word `json:"entries"`
 }
 
-func (d Dictionary) Get(mot string) string {
-	return d[mot]
+// func New() Dictionary {
+// 	return Dictionary{}
+// }
+
+func (d *Dictionary) Add(mot, definition string) error {
+	entry := Word{Mot: mot, Definition: definition}
+	d.Entries = append(d.Entries, entry)
+	return d.saveToFile()
 }
 
-func (d Dictionary) Remove(mot string) {
-	delete(d, mot)
+func (d *Dictionary) Get(mot string) (string, error) {
+	for _, entry := range d.Entries {
+		if entry.Mot == mot {
+			return entry.Definition, nil
+		}
+	}
+	return "", fmt.Errorf("Le mot '%s' n'a pas été trouvé dans le dictionnaire", mot)
 }
 
-func (d Dictionary) List() {
-	var mots []string
-	for mot := range d {
-		mots = append(mots, mot)
+func (d *Dictionary) Remove(mot string) error {
+	for i, entry := range d.Entries {
+		if entry.Mot == mot {
+			d.Entries = append(d.Entries[:i], d.Entries[i+1:]...)
+			return d.saveToFile()
+		}
+	}
+	return fmt.Errorf("Le mot '%s' n'a pas été trouvé dans le dictionnaire", mot)
+}
+
+func (d *Dictionary) List() error {
+	for _, entry := range d.Entries {
+		fmt.Printf("%s: %s\n", entry.Mot, entry.Definition)
+	}
+	return nil
+}
+
+func (d *Dictionary) saveToFile() error {
+	data, err := json.MarshalIndent(d, "", "  ")
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile("dictionary.json", data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LoadDictionaryFromFile() (*Dictionary, error) {
+	fileData, err := ioutil.ReadFile("dictionary.json")
+	if err != nil {
+		return nil, err
 	}
 
-	for _, mot := range mots {
-		fmt.Printf("%s: %s\n", mot, d[mot])
+	var dictionary Dictionary
+	err = json.Unmarshal(fileData, &dictionary)
+	if err != nil {
+		return nil, err
 	}
+	return &dictionary, nil
 }
--- main.go
+++ main.go
@@ -7,25 +7,43 @@ import (
 
 func main() {
 
-	dict := dictionary.New()
+	dict, err := dictionary.LoadDictionaryFromFile()
+	if err != nil {
+		fmt.Println("Erreur lors du chargement du dictionaire : ", err)
+		return
+	}
 
 	dict.Add("v1", "premiere valeur")
+	if err != nil {
+		fmt.Println("Erreur lors de l'ajout au dictionnaire :", err)
+	}
 	dict.Add("v2", "seconde valeur")
+	if err != nil {
+		fmt.Println("Erreur lors de l'ajout au dictionnaire :", err)
+	}
 	dict.Add("v3", "troisieme valeur")
+	if err != nil {
+		fmt.Println("Erreur lors de l'ajout au dictionnaire :", err)
+	}
 
 	mot := "v2"
-	definition := dict.Get(mot)
-	if definition != "" {
-		fmt.Printf("La définition de '%s' est : %s\n", mot, definition)
+	definition, err := dict.Get(mot)
+	if err != nil {
+		fmt.Println(err)
 	} else {
-		fmt.Printf("Le mot '%s' n'a pas été trouvé dans le dictionnaire.\n", mot)
+		fmt.Printf("La définition de '%s' est : %s\n", mot, definition)
 	}
-	fmt.Printf("Definition de %s: %s\n", mot, dict.Get(mot))
 
 	motToRemove := "v3"
 	fmt.Printf("%s a été enlever du dictionaire...\n", motToRemove)
-	dict.Remove(motToRemove)
+	err = dict.Remove(motToRemove)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	fmt.Println("\nListe des mots et de leurs définitions: ")
-	dict.List()
+	err = dict.List()
+	if err != nil {
+		fmt.Println("Erreur lors de l'affichage du dictionnaire :", err)
+	}
 }
