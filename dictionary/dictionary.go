855ff5026522c565137f712ad1c50be5069811d0
Arold Ngouani Yapteu <aroldngouani@MacBook-Air-de-Arold.local>
2023-12-20 12:58:10 +0100

    gestion concurrente avec channel en go

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -12,37 +12,52 @@ type Word struct {
 }
 
 type Dictionary struct {
-	Entries []Word `json:"entries"`
+	Entries    []Word `json:"entries"`
+	addChannel chan Word
+	delChannel chan string
 }
 
-// func New() Dictionary {
-// 	return Dictionary{}
+func New() *Dictionary {
+	d := &Dictionary{
+		addChannel: make(chan Word),
+		delChannel: make(chan string),
+	}
+	go d.processChannels()
+	return d
+}
+
+// func (d *Dictionary) Add(mot, definition string) error {
+// 	entry := Word{Mot: mot, Definition: definition}
+// 	d.Entries = append(d.Entries, entry)
+// 	return d.saveToFile()
 // }
 
-func (d *Dictionary) Add(mot, definition string) error {
-	entry := Word{Mot: mot, Definition: definition}
-	d.Entries = append(d.Entries, entry)
-	return d.saveToFile()
+func (d *Dictionary) Add(mot, definition string) {
+	d.addChannel <- Word{Mot: mot, Definition: definition}
 }
 
-func (d *Dictionary) Get(mot string) (string, error) {
-	for _, entry := range d.Entries {
-		if entry.Mot == mot {
-			return entry.Definition, nil
-		}
-	}
-	return "", fmt.Errorf("Le mot '%s' n'a pas été trouvé dans le dictionnaire", mot)
+func (d *Dictionary) Remove(mot string) {
+	d.delChannel <- mot
 }
 
-func (d *Dictionary) Remove(mot string) error {
-	for i, entry := range d.Entries {
-		if entry.Mot == mot {
-			d.Entries = append(d.Entries[:i], d.Entries[i+1:]...)
-			return d.saveToFile()
-		}
-	}
-	return fmt.Errorf("Le mot '%s' n'a pas été trouvé dans le dictionnaire", mot)
-}
+// func (d *Dictionary) Get(mot string) (string, error) {
+// 	for _, entry := range d.Entries {
+// 		if entry.Mot == mot {
+// 			return entry.Definition, nil
+// 		}
+// 	}
+// 	return "", fmt.Errorf("Le mot '%s' n'a pas été trouvé dans le dictionnaire", mot)
+// }
+
+// func (d *Dictionary) Remove(mot string) error {
+// 	for i, entry := range d.Entries {
+// 		if entry.Mot == mot {
+// 			d.Entries = append(d.Entries[:i], d.Entries[i+1:]...)
+// 			return d.saveToFile()
+// 		}
+// 	}
+// 	return fmt.Errorf("Le mot '%s' n'a pas été trouvé dans le dictionnaire", mot)
+// }
 
 func (d *Dictionary) List() error {
 	for _, entry := range d.Entries {
@@ -75,4 +90,23 @@ func LoadDictionaryFromFile() (*Dictionary, error) {
 		return nil, err
 	}
 	return &dictionary, nil
+}
+
+func (d *Dictionary) processChannels() {
+	for {
+		select {
+		case motToAdd := <-d.addChannel:
+			d.Entries = append(d.Entries, motToAdd)
+			d.saveToFile()
+		case motToRemove := <-d.delChannel:
+			for i, entry := range d.Entries {
+				if entry.Mot == motToRemove {
+					d.Entries = append(d.Entries[:i], d.Entries[i+1:]...)
+					d.saveToFile()
+					break
+				}
+			}
+		}
+
+	}
 }
--- main.go
+++ main.go
@@ -3,47 +3,66 @@ package main
 import (
 	"dictionnaire/dictionary"
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	dict, err := dictionary.LoadDictionaryFromFile()
-	if err != nil {
-		fmt.Println("Erreur lors du chargement du dictionaire : ", err)
-		return
-	}
+	dict := dictionary.New()
+	// if err != nil {
+	// 	fmt.Println("Erreur lors du chargement du dictionaire : ", err)
+	// 	return
+	// }
 
-	dict.Add("v1", "premiere valeur")
-	if err != nil {
-		fmt.Println("Erreur lors de l'ajout au dictionnaire :", err)
-	}
-	dict.Add("v2", "seconde valeur")
-	if err != nil {
-		fmt.Println("Erreur lors de l'ajout au dictionnaire :", err)
-	}
-	dict.Add("v3", "troisieme valeur")
-	if err != nil {
-		fmt.Println("Erreur lors de l'ajout au dictionnaire :", err)
-	}
+	// dict.Add("v1", "premiere valeur")
+	// if err != nil {
+	// 	fmt.Println("Erreur lors de l'ajout au dictionnaire :", err)
+	// }
+	// dict.Add("v2", "seconde valeur")
+	// if err != nil {
+	// 	fmt.Println("Erreur lors de l'ajout au dictionnaire :", err)
+	// }
+	// dict.Add("v3", "troisieme valeur")
+	// if err != nil {
+	// 	fmt.Println("Erreur lors de l'ajout au dictionnaire :", err)
+	// }
 
-	mot := "v2"
-	definition, err := dict.Get(mot)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("La définition de '%s' est : %s\n", mot, definition)
-	}
+	go func() {
+		dict.Add("v1", "premiere valeur")
+		time.Sleep(1 * time.Second)
+		// dict.Remove("v1")
+	}()
 
-	motToRemove := "v3"
-	fmt.Printf("%s a été enlever du dictionaire...\n", motToRemove)
-	err = dict.Remove(motToRemove)
-	if err != nil {
-		fmt.Println(err)
-	}
+	go func() {
+		dict.Add("v2", "seconde valeur")
+		time.Sleep(2 * time.Second)
+		// dict.Remove("v2")
+	}()
+
+	go func() {
+		dict.Add("v3", "troisieme valeur")
+		time.Sleep(3 * time.Second)
+		dict.Remove("v3")
+	}()
+
+	// mot := "v2"
+	// definition, err := dict.Get(mot)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Printf("La définition de '%s' est : %s\n", mot, definition)
+	// }
+
+	time.Sleep(5 * time.Second)
+
+	// motToRemove := "v3"
+	// fmt.Printf("%s a été enlever du dictionaire...\n", motToRemove)
+	// err = dict.Remove(motToRemove)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
 	fmt.Println("\nListe des mots et de leurs définitions: ")
-	err = dict.List()
-	if err != nil {
-		fmt.Println("Erreur lors de l'affichage du dictionnaire :", err)
-	}
+	dict.List()
+
 }
